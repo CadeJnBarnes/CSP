f117544a689dccd4dddb812f553880450aa00b79
Cade Barnes <31513801+CadeJnBarnes@users.noreply.github.com>
2017-11-29 08:14:30 -0700

    Added code to internet detail view contoller.

--- Cade_CSP/Cade_CSP/Controller/InternetDetailViewController.swift
+++ Cade_CSP/Cade_CSP/Controller/InternetDetailViewController.swift
@@ -1,9 +1,30 @@
 //
-//  InternetDetailView.swift
+//  InternetDetailViewController.swift
 //  Cade_CSP
 //
 //  Created by Barnes, Cade on 11/29/17.
 //  Copyright © 2017 CTEC. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import WebKit
+
+public class InternetDetailViewController: UIViewController
+{
+    @IBOutlet weak var textView: UILabel!
+    @IBOutlet weak var webViewer: WKWebView!
+    
+    
+    
+    override public func viewDidLoad()
+    {
+        super.viewDidLoad()
+        setup()
+    }
+    
+    private func setup() ->Void
+    {
+        
+    }
+
+}

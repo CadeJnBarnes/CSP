88c01385a851849fd733bf85052486d896b450cb
Cade Barnes <31513801+CadeJnBarnes@users.noreply.github.com>
2017-12-01 09:30:39 -0700

    Added segue code.

--- Cade_CSP/Cade_CSP/Controller/InternetMasterViewController.swift
+++ Cade_CSP/Cade_CSP/Controller/InternetMasterViewController.swift
@@ -72,4 +72,34 @@ public class InternetMasterViewController : UITableViewController
         
         return cell
     }
+    
+    override public func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        if segue.identifier! == "showDetail"
+        {
+            if let indexPath = self.tableView.indexPathForSelectedRow
+            {
+                let urlString = addresses[indexPath.row]
+                let pageText : String
+                
+                if indexPath.row == 0
+                {
+                    //TODO: replace with definitions
+                    pageText = "All definitions I wrote..."
+                }
+                else
+                {
+                    pageText = internetTopics[indexPath.row]
+                }
+                let controller = segue.destination as! InternetDetailViewController
+                
+                controller.detailAddress = urlString
+                controller.detailText = pageText
+                controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
+                controller.navigationItem.leftItemsSupplementBackButton = true
+                
+                
+            }
+        }
+    }
 }

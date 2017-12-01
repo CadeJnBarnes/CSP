08c059665dec27ce384e9f800d6eeeefd2999bd1
Cade Barnes <31513801+CadeJnBarnes@users.noreply.github.com>
2017-12-01 09:16:49 -0700

    Finished internet detail view controller for now.

--- Cade_CSP/Cade_CSP/Controller/InternetDetailViewController.swift
+++ Cade_CSP/Cade_CSP/Controller/InternetDetailViewController.swift
@@ -14,17 +14,63 @@ public class InternetDetailViewController: UIViewController
     @IBOutlet weak var textView: UILabel!
     @IBOutlet weak var webViewer: WKWebView!
     
-    
-    
-    override public func viewDidLoad()
-    {
-        super.viewDidLoad()
-        setup()
-    }
-    
     private func setup() ->Void
     {
         
     }
 
+    var detailAddress : String?
+    {
+        didSet
+        {
+            configureDetailView()
+        }
+    }
+    
+    var detailText : String?
+    {
+        didSet
+        {
+            configureDetailView()
+        }
+    }
+    override public func viewDidLoad()
+    {
+        super.viewDidLoad()
+        configureDetailView()
+    }
+
+    private func configureDetailView() -> Void
+    {
+        if detailAddress != nil
+        {
+            if let currentWebView = webViewer
+            {
+                let currentURL = URL(string: detailAddress!)
+                let currentWebRequest = URLRequest(url: currentURL!)
+                currentWebView.load(currentWebRequest)
+            }
+        }
+        else
+        {
+            if let currentWebView = webViewer
+            {
+                let currentURL = URL(string: "https://www.cnn.com")
+                currentWebView.load(URLRequest(url:currentURL!))
+            }
+        }
+       if detailText != nil
+       {
+        if let currentText = textView
+        {
+            currentText.text = detailText
+        }
+    }
+        else
+       {
+        if let currentText = textView
+        {
+            currentText.text = "Cade's CSP app internet screen"
+        }
+    
 }

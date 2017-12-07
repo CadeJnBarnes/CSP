a173a45342c5ded9f9602040e65370ceb902b41d
Cade Barnes <31513801+CadeJnBarnes@users.noreply.github.com>
2017-12-07 09:01:47 -0700

    Added a new class called Art Cell.

---
+++ Cade_CSP/Cade_CSP/Controller/ArtCell.swift
@@ -0,0 +1,17 @@
+//
+//  ArtCell.swift
+//  Cade_CSP
+//
+//  Created by Barnes, Cade on 12/7/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+import UIKit
+
+public class ArtCell: UICollectionViewCell
+{
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var imageName: UILabel!
+}
